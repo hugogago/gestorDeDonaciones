e6ee21d54817d7894c376b89a5ca65d086302036
Hugo Gago <hugogago28@gmail.com>
2025-10-02 17:19:18 +0200

    Js empezado

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+let organizaciones = [
+    {nombre: "Cruz Roja", donacion: 5 },
+    {nombre: "UNICEF", donacion: 3 },
+    {nombre: "Médicos Sin Fronteras", donacion: 2 },
+    {nombre: "Save the Children", donacion: 1 },
+    {nombre: "WWF", donacion: 4 },
+    {nombre: "Greenpeace", donacion: 8 },
+    {nombre: "Caritas", donacion: 2 },
+    {nombre: "Manos Unidas", donacion: 5 },
+    {nombre: "Aldeas Infantiles", donacion: 6 },
+    {nombre: "Fundación Vicente Ferrer", donacion: 4 }   
+]
